1f2ed8f1dc487164dcd5679b9141f00644965155
KarenAlondra <107883947+KarenAlondra@users.noreply.github.com>
2022-07-05 20:17:06 -0400

    Add files via upload

---
+++ JS_Intro_.js
@@ -0,0 +1,65 @@
+
+// Very Easy
+
+
+var person1 = " The difference between ";
+var num1 = 8765;
+var person2 = " and ";
+var num2 = 5673;
+var person3 = " is ";
+
+console.log(person1 + num1 + person2 + num2 + person3 + (num1 - num2));
+
+
+// Easy
+
+
+var name1 = "Alessandro";
+var name2 = "Michelangelo";
+
+var num1 = 10;
+var num2 = 12;
+
+
+if (name1 > name2){
+    console.log("The name " + name1 + " is longer than " + name2 + " by " + (num1 - num2) + " characters.");
+}else {
+    console.log("The name " + name1 + " is shorter than " + name2 + " by " + (num2 - num1) + " characters.");
+}
+
+
+// Medium
+
+
+var greeting1 = prompt("Say hello in all uppercase");
+console.log(greeting = "You are shouting");
+
+var greeting2 = prompt("Say hello in all lowercase");
+console.log(greeting2 = "You are whispering");
+
+var greeting3 = prompt("Say hello in normal writing");
+console.log(greeting3 = "You are talking normally");
+    
+
+// Hard
+
+
+var x = 50, y = 30;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+
+// Very Hard
+
+var x = prompt("Enter a Value");
+var y = prompt("Enter another Value");
+var num1 = parseInt(x);
+var num2 = parseInt(y);
+
+console.log(x+y); // I don't know what is wrong with this one :(  //
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
